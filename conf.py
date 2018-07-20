a91477f5f4a16d492d936f3f24138036d6dbb958
deltaVphi <madevilsniper.games@gmail.com>
2018-07-20 15:13:18 -0400

    Updated theme and ebook publisher info

--- conf.py
+++ conf.py
@@ -75,7 +75,11 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -159,10 +163,10 @@ texinfo_documents = [
 # -- Options for Epub output -------------------------------------------------
 
 # Bibliographic Dublin Core info.
-epub_title = project
-epub_author = author
-epub_publisher = author
-epub_copyright = copyright
+epub_title = u'Brothers of Tyr'
+epub_author = u'Brothers of Tyr Council'
+epub_publisher = u'Brothers of Tyr Council'
+epub_copyright = u'2018, Brothers of Tyr Council'
 
 # The unique identifier of the text. This can be a ISBN number
 # or the project homepage.
